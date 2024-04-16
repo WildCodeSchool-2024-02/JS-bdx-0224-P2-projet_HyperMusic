863889cba4b73fb603b1228ddaba051d0dd76868
PAUL BOURY <paulboury17@gmail.com>
2024-04-16 16:50:48 +0200

    Desktop version done with the css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,32 @@
-function Footer (){
-    return (
-            <footer className="footer">
-                <img className="homeIcone" src="src\assets\images/Home.svg" alt="back to the homepage" />
-                <img className="searchIcone" src="src/assets/images/Search.svg" alt="go to research page" />
-                <img className="shuffleIcone" src="src/assets/images/Shuffle.svg" alt="go to discovery page" />
-                <p>Home</p>
-                <p>Research</p>
-                <p>Discovery</p> 
-            </footer>
-    )
+function Footer() {
+  return (
+    <>
+      <nav className="navBarBottom">
+        <img
+          className="homeIcone"
+          src="src\assets\images/Home.svg"
+          alt="back to the homepage"
+        />
+        <img
+          className="searchIcone"
+          src="src/assets/images/Search.svg"
+          alt="go to research page"
+        />
+        <img
+          className="shuffleIcone"
+          src="src/assets/images/Shuffle.svg"
+          alt="go to discovery page"
+        />
+        <p>Home</p>
+        <p>Research</p>
+        <p>Discovery</p>
+      </nav>
+      <footer>
+        <p>Terms of use</p>
+        <p>Contact</p>
+        <p>About us</p>
+      </footer>
+    </>
+  );
 }
 export default Footer;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,17 @@
-function Header () {
-    return (
-            <header className="header">
-                <img className="logo" src="src/assets/images/Melody-Find-removebg-preview 3.png" alt="back to the homepage" />
-                <h1>Hypermusic</h1>
-            </header>
-    )
+function Header() {
+  return (
+    <section className="topPage">
+      <header>
+        <img
+          className="logo"
+          src="src/assets/images/Melody-Find-removebg-preview 3.png"
+          alt="back to the homepage"
+        />
+        <h1>Hypermusic</h1>
+      </header>
+      <p>Research</p>
+      <p>Discovery</p>
+    </section>
+  );
 }
 export default Header;
