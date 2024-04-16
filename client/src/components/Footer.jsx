805c8766102da5d0ea5afb8b716b7cdbe6589a81
PAUL BOURY <paulboury17@gmail.com>
2024-04-16 11:07:10 +0200

    header and footer done with css but without links

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer (){
+    return (
+            <footer className="footer">
+                <img className="homeIcone" src="src\assets\images/Home.svg" alt="back to the homepage" />
+                <img className="searchIcone" src="src/assets/images/Search.svg" alt="go to research page" />
+                <img className="shuffleIcone" src="src/assets/images/Shuffle.svg" alt="go to discovery page" />
+                <p>Home</p>
+                <p>Research</p>
+                <p>Discovery</p> 
+            </footer>
+    )
+}
+export default Footer;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header () {
+    return (
+            <header className="header">
+                <img className="logo" src="src/assets/images/Melody-Find-removebg-preview 3.png" alt="back to the homepage" />
+                <h1>Hypermusic</h1>
+            </header>
+    )
+}
+export default Header;
