eb04a51c8bd41306253588d60c079a0dc27e93f8
FGAT3 <feliciengamelin@yahoo.fr>
2024-04-17 23:51:04 +0200

    id in specific file and style picture artist

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import ArtistCard from "./components/ArtistCard";
+import artistIds from "./artistIds";
 
 function App() {
   const clientId = import.meta.env.VITE_CLIENT_ID;
@@ -8,6 +9,19 @@ function App() {
 
   const [artistData, setArtistData] = useState([]);
   const [accessToken, setAccessToken] = useState("");
+  const artistIdsString = artistIds.join(",");
+
+  const shuffleArray = (array) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
 
   useEffect(() => {
     const authParameters = {
@@ -34,27 +48,32 @@ function App() {
     };
 
     fetch(
-      "https://api.spotify.com/v1/artists?ids=06HL4z0CvFAxyc27GXpf02,6eUKZXaKkcviH0Ku9w2n3V,1Xyo4u8uXC1ZmMpatF05PJ,3QVolfxko2UyCOtexhVTli,5YGY8feqx7naU7z4HrwZM6,5pKCCKE2ajJHZ9KAiaK11H,6M2wZ9GZgrQXHCFfjv46we,66CXWjxzNUsdJxJ2JdwvnR,3TVXtAsR1Inumwj472S9r4,0C8ZW7ezQVs4URX5aX7Kqx,6jJ0s89eD6GaHleKKya26X,1Cs0zKBU1kc0i8ypK3B9ai,4tZwfgrHOc3mvqYlEYSvVi,3AA28KZvwAUcZuOKwyblJQ,6KImCVD70vtIoJWnq6nGn3,6vWDO969PvNqNYHIOW5v0m,0lAWpj5szCSwM4rUMHYmrr,1KCSPY1glIKqW2TotWuXOR,6XyY86QOPPrYVGvF9ch6wz,0VJIBKdqJygrupAxpSTk7q",
+      `https://api.spotify.com/v1/artists?ids=${artistIdsString}`,
       artistParameters
     )
       .then((result) => result.json())
-      .then((data) => setArtistData(data.artists.slice(0, 20)))
+      .then((data) => {
+        const shuffledArtists = shuffleArray(data.artists).slice(0, 20);
+        setArtistData(shuffledArtists);
+      })
       .catch((error) => {
         console.error("Error fetching artist data:", error);
       });
   }, []);
 
   return (
-    <section className="popular-artist">
+    <>
       <h2>Artistes à succès :</h2>
-      {artistData.map((artist) => (
-        <ArtistCard
-          key={artist.name}
-          artistName={artist.name}
-          imageUrl={artist.images[1].url}
-        />
-      ))}
-    </section>
+      <section className="popular-artist">
+        {artistData.map((artist) => (
+          <ArtistCard
+            key={artist.name}
+            artistName={artist.name}
+            imageUrl={artist.images[1].url}
+          />
+        ))}
+      </section>
+    </>
   );
 }
 
---
+++ client/src/artistIds.js
@@ -0,0 +1,24 @@
+const artistIds = [
+  "06HL4z0CvFAxyc27GXpf02",
+  "6eUKZXaKkcviH0Ku9w2n3V",
+  "1Xyo4u8uXC1ZmMpatF05PJ",
+  "3QVolfxko2UyCOtexhVTli",
+  "5YGY8feqx7naU7z4HrwZM6",
+  "5pKCCKE2ajJHZ9KAiaK11H",
+  "6M2wZ9GZgrQXHCFfjv46we",
+  "66CXWjxzNUsdJxJ2JdwvnR",
+  "3TVXtAsR1Inumwj472S9r4",
+  "0C8ZW7ezQVs4URX5aX7Kqx",
+  "6jJ0s89eD6GaHleKKya26X",
+  "1Cs0zKBU1kc0i8ypK3B9ai",
+  "4tZwfgrHOc3mvqYlEYSvVi",
+  "3AA28KZvwAUcZuOKwyblJQ",
+  "6KImCVD70vtIoJWnq6nGn3",
+  "6vWDO969PvNqNYHIOW5v0m",
+  "0lAWpj5szCSwM4rUMHYmrr",
+  "1KCSPY1glIKqW2TotWuXOR",
+  "6XyY86QOPPrYVGvF9ch6wz",
+  "0VJIBKdqJygrupAxpSTk7q",
+];
+
+export default artistIds;
