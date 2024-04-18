4e4a9b980f8e1234993290b3d751f8979b8a1794
Sacha Isambourg <sacha.isambourg01@gmail.com>
2024-04-18 15:45:06 +0200

    Functional SearchBar with API use

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,23 +1,44 @@
 import { useState } from "react";
+import PropTypes from 'prop-types'
 
-function SearchBar() {
+function SearchBar({ searchResult }) {
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
+  const handleSearch = () => {
+    if (searchTerm.trim() !== "") {
+      if (searchResult) {
+        searchResult(searchTerm);
+      }
+    }
+  };
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
   return (
     <section className="searchbar">
       <input
-        type="text"
+        type="search"
         className="searchbarInput"
         placeholder="Rechercher une musique, un artiste..."
         value={searchTerm}
         onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
       />
+      <button type="button" onClick={handleSearch}>
+       <img src="..\src\assets\images\icons8-chercher (1).svg" alt="Result" />
+      </button>
     </section>
   );
 }
+
+SearchBar.propTypes = {
+  searchResult: PropTypes.isRequired,
+};
 
 export default SearchBar;
