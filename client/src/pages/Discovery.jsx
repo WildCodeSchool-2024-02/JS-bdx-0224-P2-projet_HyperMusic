3892c732420aab26ad5235614f09fc12b746d571
PAUL BOURY <paulboury17@gmail.com>
2024-04-24 11:18:20 +0200

    working on the discovery page

--- client/src/pages/Discovery.jsx
+++ client/src/pages/Discovery.jsx
@@ -1,7 +1,75 @@
 import "../App.css";
+import { useState, useEffect } from "react";
+
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+const types = [
+  "acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music"
+]
+
+const years = [
+  "1900", "1910", "1920", "1930", "1940", "1950", "1960", "1970", "1980", "1990", "2000", "2010", "2020"
+]
 
 function Discovery() {
-  return <p>Discovery page</p>;
+
+  const [accessToken, setAccessToken] = useState("");
+
+  useEffect (() => {
+    const authParameters = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
+    }
+    fetch("https://accounts.spotify.com/api/token", authParameters)
+      .then(result => result.json())
+      .then(data => setAccessToken(data.access_token))
+
+      const discovery = {
+        method: "GET",
+        headers : {
+          "Content-type": "application/json",
+          "Authorization": `Bearer ${accessToken}`
+        }
+      }
+      fetch (`https://api.spotify.com/v1/recommendations?limit=1&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA&target_danceability=1`, discovery)
+        .then(result => result.json())
+        .then(data => (data))
+  }, [])
+
+  return (
+    <>
+        <section>
+            <label>
+
+                What type of music?
+                <select>
+
+                    {types.map((type) => (
+
+                    <option key={type}>{type}</option>
+
+                    ))}
+
+                </select>
+            </label>
+
+        </section>
+        <section>
+            <label>
+                which year do you like?
+                <select>
+                    {years.map((year) => (
+                    <option key={year}>{year}</option>
+                    ))}
+                </select>
+            </label>
+        </section>
+        </>
+  )
 }
 
 export default Discovery;
