fc16669d057be938db2caf81da2b8bab48cf8be8
sachaig <sacha.isambourg01@gmail.com>
2024-04-27 17:29:21 +0200

    entire artistPage

--- client/src/pages/ArtistPage.jsx
+++ client/src/pages/ArtistPage.jsx
@@ -1,12 +1,17 @@
 import { useParams, useLoaderData } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "../App.css";
+import { getOnlyYear } from "./HomePage";
+import AlbumCard from "../components/AlbumCard";
 
 function ArtistPage() {
   const authAccess = useLoaderData();
   const { artistId } = useParams();
   const [artistData, setArtistData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [albumArtistData, setAlbumArtistData] = useState({ items: [] });
+  const [singleArtistData, setSingleArtistData] = useState({ tracks: [] });
+
   useEffect(() => {
     fetch(`https://api.spotify.com/v1/artists/${artistId}`, authAccess)
       .then((result) => result.json())
@@ -18,17 +23,91 @@ function ArtistPage() {
         console.error("Error fetching artist data:", error);
         setLoading(false);
       });
+
+    fetch(`https://api.spotify.com/v1/artists/${artistId}/albums`, authAccess)
+      .then((result) => result.json())
+      .then((data) => {
+        console.info(data);
+        const albumWithYear = data.items.map((album) => ({
+          ...album,
+          releaseYear: getOnlyYear(album.release_date),
+        }));
+        setAlbumArtistData({ items: albumWithYear });
+        setLoading(false);
+      })
+
+      .catch((error) => {
+        console.error("Error fetching album artist data:", error);
+        setLoading(false);
+      });
+
+    fetch(
+      `https://api.spotify.com/v1/artists/${artistId}/top-tracks`,
+      authAccess
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        setSingleArtistData(data);
+        setLoading(false);
+        console.info(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching artist data:", error);
+        setLoading(false);
+      });
   }, []);
   if (loading) {
     return <h2>Loading...</h2>;
   }
   return (
-    <article>
-      <section>
-        <h2>{artistData.name}</h2>
-        <img src={artistData.images[1].url} alt="illustration artist" />
+    <>
+      <section className="album-section">
+        <h2 className="artist-name">{artistData.name}</h2>
+        <img
+          className="artist-page-image"
+          src={artistData.images[1].url}
+          alt="illustration artist"
+        />
       </section>
-    </article>
+      <h2>Albums et EPs</h2>
+      <section className="best-album">
+        {albumArtistData.items &&
+          albumArtistData.items.map((album) => (
+            <AlbumCard
+              key={album.id}
+              artistNameAlbum={
+                album.artists[0].name.slice(0, 24) +
+                (album.artists[0].name.length > 24 ? "..." : "")
+              }
+              albumName={
+                album.name.slice(0, 26) + (album.name.length > 26 ? "..." : "")
+              }
+              imageAlbum={album.images[1].url}
+              releaseDate={album.releaseYear}
+              albumId={album.id}
+            />
+          ))}
+      </section>
+      <h2>Top Singles</h2>
+      <section className="best-album">
+        {singleArtistData.tracks &&
+          singleArtistData.tracks.map((single) => (
+            <AlbumCard
+              key={single.id}
+              artistNameAlbum={
+                single.album.artists[0].name.slice(0, 26) +
+                (single.album.artists[0].length > 26 ? "..." : "")
+              }
+              albumName={
+                single.name.slice(0, 26) +
+                (single.name.length > 26 ? "..." : "")
+              }
+              imageAlbum={single.album.images[1].url}
+              albumId={single.album.id}
+            />
+          ))}
+      </section>
+    </>
   );
 }
 
